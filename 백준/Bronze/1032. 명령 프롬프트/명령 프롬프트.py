5f376bd2c1b9c272fd97bff9ac7010eaff15202d
Iskander <107216846+HeitenGane@users.noreply.github.com>
2023-07-11 06:09:13 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,26 +1,23 @@
-n = int(input())  # 파일명의 개수를 입력받습니다.
+import sys
 
-file_names = []  # 파일명을 저장할 리스트를 생성합니다.
+T = int(input())
 
-# 파일명을 입력받아 리스트에 저장합니다.
-for _ in range(n):
-    file_name = input()
-    file_names.append(file_name)
+names = []
+for tc in range(1, T+1):
+    name = sys.stdin.readline()
+    names.append(name)
+pattern = names[0]
+answer = ""
 
-pattern = file_names[0]  # 첫 번째 파일명을 패턴으로 설정합니다.
-
-answer = ""  # 변환된 파일명을 저장할 변수를 생성합니다.
-
-# 각 파일명의 문자를 비교하여 패턴을 만듭니다.
 for i in range(len(pattern)):
-    same_char = True  # 해당 위치의 문자가 모두 같은지 확인하기 위한 변수입니다.
-    for j in range(1, n):
-        if pattern[i] != file_names[j][i]:
+    same_char = True
+    for j in range(1, T):
+        if pattern[i] != names[j][i]:
             same_char = False
             break
     if same_char:
         answer += pattern[i]
     else:
-        answer += "?"
+        answer += '?'
 
-print(answer)  # 변환된 파일명을 출력합니다.
+print(answer)
