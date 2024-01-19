2c4e8f8ae2bfc3d23e17654189e44ae92a9a3141
LEE SANG HYUK/이상혁 <107216846+LeeSanghyuk36@users.noreply.github.com>
2024-01-19 16:32:09 +0900

    [Bronze IV] Title: Lunacy, Time: 220 ms, Memory: 18024 KB -BaekjoonHub

---
+++ 백준/Bronze/4714. Lunacy/Lunacy.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weightOnEarth = 0; // 지구 무게 저장하는 변수
+
+        while (true) {
+            weightOnEarth = sc.nextDouble();
+            if (weightOnEarth < 0) {
+                break;
+            }
+            double weightOnMoon = weightOnEarth * 0.167;
+            System.out.printf("Objects weighing %.2f on Earth will weigh %.2f on the moon.\n", weightOnEarth, weightOnMoon);
+        }
+    }
+}
